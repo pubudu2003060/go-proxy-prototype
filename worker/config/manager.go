236db011a57606834327b5a9433ab7a0cc1506ff
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-11-10 16:32:11 +0530

    add pool and user structs for workers

--- worker/config/manager.go
+++ worker/config/manager.go
@@ -1,1 +1,13 @@
 package config
+
+import (
+	"sync"
+
+	"github.com/pubudu2003060/go-proxy-prototype/worker/models"
+)
+
+type ConfigManager struct {
+	captainURL string
+	pools      map[string]*models.Pool
+	mu sync.RWMutex
+}
---
+++ worker/models/pool.go
@@ -0,0 +1,41 @@
+package models
+
+type Pool struct {
+	Name      string `json:"name"`
+	Continent string `json:"continent"`
+	Tag       string `json:"tag"`
+	Subdomain string `json:"subdomain"`
+	CC3       string `json:"cc3"`
+	PortStart int    `json:"port_start"`
+	PortEnd   int    `json:"port_end"`
+	Flag      int    `json:"flag"`
+	Outs      []Out  `json:"outs"`
+}
+
+type Out struct {
+	Format       string `json:"format"`
+	UpstreamPort int    `json:"upstream_port"`
+	Domain       string `json:"domain"`
+	Weight       int    `json:"weight"`
+}
+
+type CreatePoolRequest struct {
+	Name      string `json:"name" binding:"required"`
+	Continent string `json:"continent" binding:"required"`
+	Tag       string `json:"tag" binding:"required"`
+	Subdomain string `json:"subdomain" binding:"required"`
+	CC3       string `json:"cc3"`
+	PortStart int    `json:"port_start" binding:"required"`
+	PortEnd   int    `json:"port_end" binding:"required"`
+	Outs      []Out  `json:"outs" binding:"required"`
+}
+
+type UpdatePoolRequest struct {
+	Continent *string `json:"continent,omitempty" `
+	Tag       *string `json:"tag,omitempty"`
+	Subdomain *string `json:"subdomain,omitempty"`
+	CC3       *string `json:"cc3,omitempty"`
+	PortStart *int    `json:"port_start,omitempty"`
+	PortEnd   *int    `json:"port_end,omitempty"`
+	Outs      *[]Out  `json:"outs,omitempty"`
+}
